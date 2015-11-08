bac0e3c1346687e7fc268bafe61517ad24e8f08c
Etienne Marais <etienne.marais@olx.com>
2015-11-08 12:32:50 +0200

    Settings - To add the values for a settings menu. Cleaned up the code a bit. Added the new activity to the manifest.

--- app/src/main/java/com/olx/etiennemarais/popularmovies/MainActivity.java
+++ app/src/main/java/com/olx/etiennemarais/popularmovies/MainActivity.java
@@ -1,8 +1,12 @@
 package com.olx.etiennemarais.popularmovies;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
+import android.view.MenuItem;
+
+import com.olx.etiennemarais.popularmovies.Settings.SettingsActivity;
 
 public class MainActivity extends ActionBarActivity
 {
@@ -15,10 +19,21 @@ public class MainActivity extends ActionBarActivity
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if (item.getItemId() == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
